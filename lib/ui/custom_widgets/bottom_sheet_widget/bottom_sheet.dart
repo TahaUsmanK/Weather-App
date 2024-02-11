987756c229e7f00b4d83aa58557bcbc14a13861e
TahaUsmanK <143314165+TahaUsmanK@users.noreply.github.com>
2024-02-11 13:29:58 +0500

    some error fixed

--- lib/ui/custom_widgets/bottom_sheet_widget/bottom_sheet.dart
+++ lib/ui/custom_widgets/bottom_sheet_widget/bottom_sheet.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_antonx_boilerplate/ui/custom_widgets/bottom_sheet_widget/bottom_sheet_weather_forecast.dart';
+import 'package:flutter_antonx_boilerplate/ui/custom_widgets/hourly_weather/hourly_weather.dart';
+import 'package:flutter_antonx_boilerplate/ui/custom_widgets/weekly_weather_widget/weekly_weather_widget.dart';
+import 'package:flutter_antonx_boilerplate/ui/screens/home_screen/home_screen_view_model.dart';
 import 'package:glass_kit/glass_kit.dart';
+import 'package:provider/provider.dart';
 import 'draggable_bottom_sheet_nullsafety.dart';
-import 'bottom_sheet_weather_hourly.dart';
 
 class BottomSheets extends StatefulWidget {
   const BottomSheets({Key? key}) : super(key: key);
@@ -14,6 +16,8 @@ class BottomSheets extends StatefulWidget {
 class _BottomSheetsState extends State<BottomSheets>
     with SingleTickerProviderStateMixin {
   late TabController _controller;
+  final double _minHeight = 180;
+  final double _maxHeight = 600;
 
   @override
   void initState() {
@@ -25,58 +29,90 @@ class _BottomSheetsState extends State<BottomSheets>
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width * 0.9;
 
-    return Stack(
-      children: [
-        DraggableBottomSheet(
-          previewChild: Center(
-            child: GlassContainer(
-              width: width,
-              height: MediaQuery.of(context).size.height,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-              blur: 7,
-              borderColor: Colors.transparent,
-              alignment: Alignment.topCenter, // Aligns the previewChild on top
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color(0xff612FAB).withAlpha(100),
-                  Color(0xff612FAB).withAlpha(50),
-                ],
-                stops: [0.3, 1],
-              ),
-              child: WeatherHourlyDaily(),
-            ),
+    return DraggableBottomSheet(
+      previewChild: _buildHourlyAndWeeklyForecast(width),
+      expandedChild: _buildHourlyAndWeeklyForecast(width),
+      minExtent: _minHeight,
+      maxExtent: _maxHeight,
+    );
+  }
+
+  Widget _buildHourlyAndWeeklyForecast(double width) {
+    return GlassContainer(
+      width: width,
+      height: _maxHeight,
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(20),
+        topRight: Radius.circular(20),
+      ),
+      blur: 7,
+      borderColor: Colors.transparent,
+      alignment: Alignment.topCenter,
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Color(0xff612FAB).withAlpha(100),
+          Color(0xff612FAB).withAlpha(50),
+        ],
+        stops: [0.3, 1],
+      ),
+      child: Column(
+        children: [
+          TabBar(
+            controller: _controller,
+            labelColor: Colors.grey.shade300,
+            indicatorColor: Colors.white,
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorWeight: 3,
+            tabs: [
+              Tab(text: 'Hourly Forecast'),
+              Tab(text: 'Weekly Forecast'),
+            ],
           ),
-          expandedChild: GlassContainer(
-            width: width,
-            height: MediaQuery.of(context).size.height,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            ),
-            blur: 5,
-            borderColor: Colors.transparent,
-            alignment: Alignment.topCenter, // Aligns the expandedChild on top
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              colors: [
-                Color(0xff612FAB).withAlpha(100),
-                Color(0xff612FAB).withAlpha(50),
+          Expanded(
+            child: TabBarView(
+              controller: _controller,
+              children: [
+                _buildHourlyForecast(),
+                _buildWeeklyForecast(),
               ],
-              stops: [0.3, 1],
             ),
-            child: BottomSheetWeather(),
           ),
-          minExtent: 180,
-          maxExtent: MediaQuery.of(context)
-              .size
-              .height, // Occupies entire screen height
-        ),
-      ],
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHourlyForecast() {
+    return Consumer<HomeViewModel>(
+      builder: (context, viewModel, _) {
+        final weatherData = viewModel.weatherData;
+        if (weatherData == null || weatherData.isEmpty) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          final hourlyData =
+              (weatherData['hourly'] as List?)?.cast<Map<String, dynamic>>() ??
+                  [];
+          return HourlyWeatherWidget(hourlyWeatherData: hourlyData);
+        }
+      },
+    );
+  }
+
+  Widget _buildWeeklyForecast() {
+    return Consumer<HomeViewModel>(
+      builder: (context, viewModel, _) {
+        final weatherData = viewModel.weatherData;
+        if (weatherData == null || weatherData.isEmpty) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          final weeklyData =
+              (weatherData['daily'] as List?)?.cast<Map<String, dynamic>>() ??
+                  [];
+          return WeeklyWeatherWidget(weeklyWeatherData: weeklyData);
+        }
+      },
     );
   }
 }
